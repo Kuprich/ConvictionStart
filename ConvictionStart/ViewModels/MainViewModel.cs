531270823f8000a2e789ddabf53689920f24f946
Kupriyanov Ilya <ilya-kyp@yandex.ru>
2024-05-03 15:19:15 +0300

    updated temtlate

---
+++ ConvictionStart/ViewModels/MainViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ConvictionStart.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string OnTopMessage { get; set; } = "Следующие файлы доступны для обновления";
+    public string UpdateButtonText { get; set; } = "Обновить";
+}
--- ConvictionStart/ViewModels/MainWindowViewModel.cs
+++ ConvictionStart/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace ConvictionStart.ViewModels
+﻿using Avalonia.Remote.Protocol;
+
+namespace ConvictionStart.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public MainViewModel MainView { get; }
+        public MainWindowViewModel()
+        {
+            MainView = new MainViewModel();
+        }
     }
 }
